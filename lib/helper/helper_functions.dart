bc3c80f0f46b5c7289216aa86075c2ec48eced5c
Priyanshu Gupta <guptapriyanshu71@gmail.com>
2022-02-06 21:08:49 +0530

    add permissions for geolocator

--- lib/helper/helper_functions.dart
+++ lib/helper/helper_functions.dart
@@ -2,15 +2,38 @@ import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 
 Future<String> getLocation() async {
-  final position = await Geolocator.getCurrentPosition(
-    desiredAccuracy: LocationAccuracy.high,
-  );
+  final position = await _determinePosition();
   final placemarks = await placemarkFromCoordinates(
     position.latitude,
     position.longitude,
   );
   final placemark = placemarks[0];
-  final String formattedAddress =
+  final formattedAddress =
       "${placemark.street}, ${placemark.subLocality}, ${placemark.locality}, ${placemark.administrativeArea}, ${placemark.country}";
   return formattedAddress;
+}
+
+Future<Position> _determinePosition() async {
+  final serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+
+  var permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.',
+    );
+  }
+
+  return Geolocator.getCurrentPosition(
+    desiredAccuracy: LocationAccuracy.high,
+  );
 }
